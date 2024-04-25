16f203907d91d9f434f8ebd17ce388690761eec3
Thejesh-ivoyant <127406875+Thejesh-ivoyant@users.noreply.github.com>
2024-04-25 13:57:55 +0530

    logo removed

--- ihrm-ui/src/App.tsx
+++ ihrm-ui/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from '.';
 
 import './App.css';
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
          likhith 123
         </p>

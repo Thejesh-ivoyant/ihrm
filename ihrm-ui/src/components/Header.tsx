edc40388bdb5896dc792831d22b09595c4812f3b
thejesh <thejeshmulinja123@gmail.com>
2024-05-01 01:50:49 +0530

    added dashboard structure

---
+++ ihrm-ui/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>My Dashboard</h1>
+      <button>Settings</button>
+    </header>
+  );
+}
+
+export default Header;
---
+++ ihrm-ui/src/components/MainContent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main className="main-content">
+        
+      <div>Main Content Area</div>
+      <div>This is where your main dashboard content will go.</div>
+    </main>
+  );
+}
+
+export default MainContent;
